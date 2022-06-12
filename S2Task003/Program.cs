ace5466565b63d12481a547d1c59be16707d96d4
Jeiojey <signofaries@gmail.com>
2022-06-12 16:10:55 +0300

    Task003

---
+++ S2Task003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите цифру дня недели: ");
+int dayNumber = Convert.ToInt32(ReadLine());
+
+string answer = dayNumber == 6 || dayNumber == 7 ? "да" : "нет";
+WriteLine(answer);
